9433198731138e8b6a1fca87c2b86fd2a4930b65
lghilardi <ghilardilogan@gmail.com>
2020-01-20 11:41:25 +0100

    uy

--- index5.php
+++ index5.php
@@ -23,39 +23,37 @@
 </head>
 
 <body style="background-color: grey;">
-    <div class="container-fluid" style="background-color: grey;">
+<div class="container-fluid" style="background-color: grey;">
 
-        <?php include 'menu.php' ;
-    
-     
-if (!isset($_SESSION['pseudo']))
-{
-    echo '<center><font color="red" size="4"><b>Vous devez vous connecter pour acceder à la page </center></font><br />';
-}
-else
-{ 
-try
-{
-	// On se connecte à MySQL
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-	// En cas d'erreur, on affiche un message et on arrête tout
-        die('Erreur : '.$e->getMessage());
-}
-        ?>
-    </div>
-    <?php } ?>
+    <?php include 'menu.php';
 
-    <?php if(isset($_POST['go'])) {
-        echo 'ouiiiiii';
 
+    if (!isset($_SESSION['pseudo']))
+    {
+        echo '<center><font color="red" size="4"><b>Vous devez vous connecter pour acceder à la page </center></font><br />';
+    }
+    else
+    {
+    try {
+        // On se connecte à MySQL
+        $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+    } catch (Exception $e) {
+        // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : ' . $e->getMessage());
     }
     ?>
-    <form method="post" action="index5.php">
-        <input type="submit" value="envoyer" name="go" />
-    </form>
+</div>
+
+
+<?php if (isset($_POST['go'])) {
+    echo 'ouiiiiii';
+
+}
+?>
+<form method="post" action="index5.php">
+    <input type="submit" value="envoyer" name="go"/>
+</form>
+<?php } ?>
 </body>
 
 </html>
